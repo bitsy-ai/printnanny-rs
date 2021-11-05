70972f559086370a7d8162203b6c9dbbd6dffbe7
Leigh Johnson <hi@leighjohnson.me>
2021-11-04 22:18:57 -0700

    use bind iface :8331

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,8 @@ async fn main() -> Result<()> {
         .subcommand(SubCommand::with_name("ansible-extra-vars")
             .about("Output device config as Ansible Facts"))
         .subcommand(SubCommand::with_name("http")
-            .about("Serves HTTP requests. Used for device registration and status checks."))
+            .about("Serves HTTP requests. Used for device registration and status checks.")
+        )
         .subcommand(SubCommand::with_name("setup")
             .about("Connect your Print Nanny account"))
         .subcommand(SubCommand::with_name("reset")
@@ -65,10 +66,11 @@ async fn main() -> Result<()> {
             config.refresh().await?;
             
         },
-        ("http", Some(_sub_m)) => {
+        ("http", Some(sub_m)) => {
             let routes = warp::path!("config")
                 .map(handle_probe);
-            warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+            let addr = std::net::Ipv4Addr::new(0,0,0,0);
+            warp::serve(routes).run((addr, 8331)).await;
         },
         ("reset", Some(_sub_m)) => {
             let mut prompter = SetupPrompter::new()?;
