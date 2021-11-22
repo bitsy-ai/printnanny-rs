ae076c80b5c12128f03883dd2f4b3112a2080331
Leigh Johnson <leigh@bitsy.ai>
2021-11-21 16:54:03 -0800

    run cargo update to generate Cargo.lock in bump release script

--- tools/bump-release.sh
+++ tools/bump-release.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -eu
 bump2version --current-version $(cat version.txt) --new-version "$1" patch
-cargo check
+cargo update
+git add Cargo.lock
 git commit --amend --no-edit
 git push
 git push --tags
