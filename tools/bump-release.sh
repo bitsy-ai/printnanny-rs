be2f3684ec63a97a0e8a38f78d40af563de098f6
Leigh Johnson <leigh@bitsy.ai>
2021-11-21 17:14:28 -0800

    check in Cargo revs

--- tools/bump-release.sh
+++ tools/bump-release.sh
@@ -2,7 +2,7 @@
 set -eu
 bump2version --current-version $(cat version.txt) --new-version "$1" patch
 cargo update
-git add Cargo.lock
+git add -A
 git commit --amend --no-edit
 git push
 git push --tags
