5d0932e3efc85f3dd4d53d5c47692d8f26ef33b4
Leigh Johnson <leigh@bitsy.ai>
2021-12-11 18:49:04 -0800

    support more than 1 device download path for testing various states, download ca certs for testing

--- tools/download-license.sh
+++ tools/download-license.sh
@@ -11,9 +11,17 @@ get_test_api_token(){
 PRINTNANNY_API_URL="${PRINTNANNY_API_URL:-http://aurora:8000}"
 PRINTNANNY_API_TOKEN="${PRINTNANNY_API_TOKEN:-$(get_test_api_token)}"
 PRINTNANNY_DEVICE_HOSTNAME="${PRINTNANNY_DEVICE_HOSTNAME:-printnanny}"
-PRINTNANNY_INSTALL_DIR="${PRINTNANNY_INSTALL_DIR:-.tmp/data}"
+PRINTNANNY_INSTALL_DIR="${PRINTNANNY_INSTALL_DIR:-.tmp/printnanny}"
+PRINTNANNY_DATA_DIR="${PRINTNANNY_DATA_DIR:-.tmp/printnanny/data}"
+PRINTNANNY_CACERT_DIR="${PRINTNANNY_CACERT_DIR:-.tmp/printnanny/ca-certificates}"
 
 echo "$PRINTNANNY_API_TOKEN"
+
+download_ca_certs(){
+    wget https://pki.goog/gtsltsr/gtsltsr.crt -P "$PRINTNANNY_CACERT_DIR"
+    wget https://pki.goog/gsr4/GSR4.crt -P "$PRINTNANNY_CACERT_DIR"
+}
+
 get_test_device(){
     curl -sX "GET" \
         "$PRINTNANNY_API_URL/api/devices/$PRINTNANNY_DEVICE_HOSTNAME" \
@@ -28,7 +36,7 @@ get_test_device_id(){
 download_license(){
     device_id=$(get_test_device_id)
     echo "Fetching license for device_id=$device_id"
-    filename="$PRINTNANNY_INSTALL_DIR/printnanny_license.zip"
+    filename="$PRINTNANNY_DATA_DIR/printnanny_license.zip"
     curl -sX "GET" \
         "$PRINTNANNY_API_URL/api/devices/$device_id/generate-license/" \
         -H "accept: application/json" \
@@ -37,6 +45,7 @@ download_license(){
     echo "Created $filename"
 }
 
-mkdir -p $PRINTNANNY_INSTALL_DIR
+mkdir -p $PRINTNANNY_DATA_DIR
+download_ca_certs
 download_license
-cd "$PRINTNANNY_INSTALL_DIR" && unzip printnanny_license.zip
+cd "$PRINTNANNY_DATA_DIR" && unzip printnanny_license.zip
