df2ca4519e9ec2061ee6dfa09a2e960abf683b5c
Leigh Johnson <leigh@bitsy.ai>
2022-01-06 10:49:08 -0800

    Passwordless device registration flow (Step 1) (#54)
    * serve form on port 9001
    
    * step 1 form
    
    * rename to dash crate, create sqlite db if missing
    
    * fix warnings and rm data.db
    
    * add .db to gitignore
    
    * fix type refs

---
+++ dash/src/main.rs
@@ -0,0 +1,127 @@
+#[macro_use] extern crate rocket;
+use std::str::FromStr;
+
+use anyhow::{Error, Result};
+use clap::{ 
+    Arg,
+    App,
+    crate_version,
+    crate_authors,
+    crate_description
+};
+
+use rocket::http::{Status, ContentType};
+use rocket::form::{
+    Form,
+    Contextual,
+    FromForm,
+    FromFormField,
+    Context,
+};
+use rocket::fs::{FileServer, TempFile, relative};
+use rocket_auth::{ Users, User };
+use rocket_dyn_templates::Template;
+use sqlx::sqlite::{ SqlitePool, SqliteConnectOptions};
+use sqlx::prelude::ConnectOptions;
+#[derive(Debug, FromForm)]
+struct Password<'v> {
+    #[field(validate = len(6..))]
+    #[field(validate = eq(self.second))]
+    first: &'v str,
+    #[field(validate = eq(self.first))]
+    second: &'v str,
+}
+
+#[derive(Debug, FromFormField)]
+enum Rights {
+    Public,
+    Reserved,
+    Exclusive,
+}
+
+#[derive(Debug, FromFormField)]
+enum Category {
+    Biology,
+    Chemistry,
+    Physics,
+    #[field(value = "CS")]
+    ComputerScience,
+}
+
+#[derive(Debug, FromForm)]
+struct Submission<'v> {
+    #[field(validate = len(1..))]
+    title: &'v str,
+    #[field(validate = len(1..=250))]
+    r#abstract: &'v str,
+    #[field(validate = ext(ContentType::PDF))]
+    file: TempFile<'v>,
+    #[field(validate = len(1..))]
+    category: Vec<Category>,
+    rights: Rights,
+    ready: bool,
+}
+
+#[derive(Debug, FromForm)]
+struct Signup<'v> {
+    #[field(validate = contains('@').or_else(msg!("invalid email address")))]
+    email: &'v str,
+    analytics: bool,
+}
+
+
+#[get("/")]
+fn index(option: Option<User>) -> Template {
+    if let Some(user) = option {
+        Template::render("index", user)
+    } else {
+        Template::render("login", &Context::default())
+    }
+}
+
+// NOTE: We use `Contextual` here because we want to collect all submitted form
+// fields to re-render forms with submitted values on error. If you have no such
+// need, do not use `Contextual`. Use the equivalent of `Form<Submit<'_>>`.
+#[post("/login", data = "<form>")]
+fn submit<'r>(form: Form<Contextual<'r, Signup<'r>>>) -> (Status, Template) {
+    let template = match form.value {
+        Some(ref submission) => {
+            println!("submission: {:#?}", submission);
+            Template::render("success", &form.context)
+        }
+        None => Template::render("index", &form.context),
+    };
+
+    (form.context.status(), template)
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app_name = "printnanny-dash";
+    let app = App::new(app_name)
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(Arg::new("db")
+        .help("Path to sqlite.db")
+        .default_value("sqlite://data.db")
+        .takes_value(true));
+    
+    let app_m = app.get_matches();
+    let db = app_m.value_of("db").unwrap();
+
+    SqliteConnectOptions::from_str(&db)?
+        .create_if_missing(true)
+        .connect().await?;
+    let conn = SqlitePool::connect(&db).await?;
+    let users: Users = conn.into();
+    users.create_table().await?;
+
+    rocket::build()
+        .mount("/", routes![index, submit])
+        .attach(Template::fairing())
+        .mount("/", FileServer::from(relative!("/static")))
+        .manage(users)
+        .launch().await?;
+    Ok(())
+}
