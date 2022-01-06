bb8cd44163a3e7374f66470149f9294a2fc12eba
Leigh Johnson <leigh@bitsy.ai>
2022-01-05 21:36:04 -0800

    rename to dash crate, create sqlite db if missing

--- dash/src/main.rs
+++ dash/src/main.rs
@@ -1,11 +1,23 @@
 #[macro_use] extern crate rocket;
+use std::str::FromStr;
+
+use anyhow::{ Result };
+use clap::{ 
+    Arg,
+    App,
+    crate_version,
+    crate_authors,
+    crate_description
+};
 
 use rocket::http::{Status, ContentType};
 use rocket::form::{Form, Contextual, FromForm, FromFormField, Context};
 use rocket::fs::{FileServer, TempFile, relative};
-
+use rocket_auth::{ Users, User };
+use rocket_sync_db_pools::Config;
 use rocket_dyn_templates::Template;
-
+use sqlx::sqlite::{ SqlitePool, SqliteConnectOptions};
+use sqlx::prelude::ConnectOptions;
 #[derive(Debug, FromForm)]
 struct Password<'v> {
     #[field(validate = len(6..))]
@@ -47,29 +59,32 @@ struct Submission<'v> {
 
 #[derive(Debug, FromForm)]
 struct Account<'v> {
-    #[field(validate = len(1..))]
-    name: &'v str,
-    password: Password<'v>,
     #[field(validate = contains('@').or_else(msg!("invalid email address")))]
     email: &'v str,
+    analytics: bool,
+
 }
 
 #[derive(Debug, FromForm)]
-struct Submit<'v> {
+struct SubmitStep1<'v> {
     account: Account<'v>,
-    submission: Submission<'v>,
+    // submission: Submission<'v>,
 }
 
 #[get("/")]
-fn index() -> Template {
-    Template::render("index", &Context::default())
+fn index(option: Option<User>) -> Template {
+    if let Some(user) = option {
+        Template::render("index", &Context::default())
+    } else {
+        Template::render("login", &Context::default())
+    }
 }
 
 // NOTE: We use `Contextual` here because we want to collect all submitted form
 // fields to re-render forms with submitted values on error. If you have no such
 // need, do not use `Contextual`. Use the equivalent of `Form<Submit<'_>>`.
-#[post("/", data = "<form>")]
-fn submit<'r>(form: Form<Contextual<'r, Submit<'r>>>) -> (Status, Template) {
+#[post("/login", data = "<form>")]
+fn submit<'r>(form: Form<Contextual<'r, SubmitStep1<'r>>>) -> (Status, Template) {
     let template = match form.value {
         Some(ref submission) => {
             println!("submission: {:#?}", submission);
@@ -81,10 +96,40 @@ fn submit<'r>(form: Form<Contextual<'r, Submit<'r>>>) -> (Status, Template) {
     (form.context.status(), template)
 }
 
-#[launch]
-fn rocket() -> _ {
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app_name = "printnanny-dash";
+    let app = App::new(app_name)
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(Arg::with_name("db")
+        .help("Path to sqlite.db")
+        .default_value("sqlite://data.db")
+        .takes_value(true));
+    
+    let app_m = app.get_matches();
+    let db = app_m.value_of("db").unwrap();
+
+    let options = SqliteConnectOptions::from_str(&db)?
+        .create_if_missing(true)
+        .connect().await?;
+    let conn = SqlitePool::connect(&db).await?;
+    let users: Users = conn.into();
+    users.create_table().await?;
+
     rocket::build()
         .mount("/", routes![index, submit])
         .attach(Template::fairing())
         .mount("/", FileServer::from(relative!("/static")))
+        .manage(users)
+        .launch();
+
+    // let rocket::build()
+    //     .mount("/", routes![index, submit])
+    //     .attach(Template::fairing())
+    //     .mount("/", FileServer::from(relative!("/static")))
+    //     // .manage(users)
+    //     .launch();
+    Ok(())
 }
