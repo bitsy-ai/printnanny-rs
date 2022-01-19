547a251656ff4a1f397deca025bb6951d930ac71
Leigh Johnson <leigh@bitsy.ai>
2022-01-19 00:15:35 -0800

    get hierarchal config working

--- services/src/config.rs
+++ services/src/config.rs
@@ -1,13 +1,12 @@
 use figment::error::Result;
-use figment::value::{magic::RelativePathBuf, Dict, Map};
-use figment::{
-    providers::{Env, Format, Json, Serialized, Toml},
-    Error as FigmentError, Figment, Metadata, Profile, Provider,
-};
+use figment::providers::{Env, Format, Json, Serialized, Toml};
+use figment::value::{magic::RelativePathBuf, Dict, Map, Value};
+use figment::{Error as FigmentError, Figment, Metadata, Profile, Provider};
 use glob::glob;
-use log::{error, info};
+use log::{debug, error, info};
 use printnanny_api_client::models;
 use serde::{Deserialize, Serialize};
+use std::any::TypeId;
 use std::fs;
 use std::path::PathBuf;
 use thiserror::Error;
@@ -18,10 +17,10 @@ use crate::printnanny_api::ApiConfig;
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub struct Config {
     pub api_config: ApiConfig,
-    pub profile: Profile,
     pub path: String,
     pub device: Option<models::Device>,
     pub user: Option<models::User>,
+    pub port: i32,
 }
 pub struct ConfigError {}
 
@@ -32,45 +31,61 @@ impl Default for Config {
             bearer_access_token: None,
         };
         let path = "/opt/printnanny/default".into();
-        let profile = "default".into();
         Config {
             api_config,
-            profile,
             path,
             device: None,
             user: None,
+            port: 9001,
         }
     }
 }
 
 impl Config {
-    pub const LOCAL_PROFILE: Profile = Profile::const_new("local");
-    pub const SANDBOX_PROFILE: Profile = Profile::const_new("sandbox");
-    pub const DEFAULT_PROFILE: Profile = Profile::const_new("default");
     // See example: https://docs.rs/figment/latest/figment/index.html#extracting-and-profiles
     // Note the `nested` option on both `file` providers. This makes each
     // top-level dictionary act as a profile.
     pub fn figment() -> Figment {
-        let profile = Profile::from_env_or("PRINTNANNY_PROFILE", Self::DEFAULT_PROFILE);
-        let result = Figment::from(Config {
-            profile,
+        // let profile = Profile::from_env_or("PRINTNANNY_PROFILE", Self::DEFAULT_PROFILE);
+        let mut result = Figment::from(Config {
+            // profile,
             ..Config::default()
         })
-        .merge(Toml::file(Env::var_or("PRINTNANNY_CONFIG", "PrintNanny.toml")).nested())
-        .merge(Env::prefixed("PRINTNANNY_").ignore(&["PROFILE"]).global())
-        .select(Profile::from_env_or(
-            "PRINTNANNY_PROFILE",
-            Self::DEFAULT_PROFILE,
-        ));
-        info!("Using profile: {:?}", result.profile());
+        .merge(Toml::file(Env::var_or(
+            "PRINTNANNY_CONFIG",
+            "PrintNanny.toml",
+        )))
+        .merge(Env::prefixed("PRINTNANNY_").global());
 
-        let toml_glob = format!("{}/*.toml", &result.profile());
-        let json_glob = format!("{}/*.json", &result.profile());
+        info!("Loaded config from profile {:?}", result.profile());
+        let path: String = result
+            .find_value("path")
+            .unwrap_or(Value::from(Config::default().path))
+            .deserialize::<String>()
+            .unwrap();
 
-        for entry in glob(&toml_glob).expect("Failed to read glob pattern") {
+        let toml_glob = format!("{}/*.toml", &path);
+        let json_glob = format!("{}/*.json", &path);
+
+        let result = Config::read_path_glob::<Json>(&json_glob, result);
+        let result = Config::read_path_glob::<Toml>(&toml_glob, result);
+
+        result
+    }
+
+    fn read_path_glob<T: 'static + figment::providers::Format>(
+        pattern: &str,
+        figment: Figment,
+    ) -> Figment {
+        info!("Merging config from {:?}", &pattern);
+        let mut result = figment.clone();
+        for entry in glob(&pattern).expect("Failed to read glob pattern") {
             match entry {
-                Ok(path) => println!("{:?}", path.display()),
-                Err(e) => println!("{:?}", e),
+                Ok(path) => {
+                    info!("Merging config from {:?}", &path);
+                    result = result.clone().merge(T::file(path))
+                }
+                Err(e) => error!("{:?}", e),
             }
         }
         result
@@ -96,8 +111,7 @@ impl Config {
     /// # Example
     pub fn try_from<T: Provider>(provider: T) -> Result<Self> {
         let figment = Figment::from(provider);
-        let mut config = figment.extract::<Self>()?;
-        config.profile = figment.profile().clone();
+        let config = figment.extract::<Self>()?;
         Ok(config)
     }
 }
@@ -111,40 +125,26 @@ impl Provider for Config {
         let map: Map<Profile, Dict> = Serialized::defaults(self).data()?;
         Ok(map)
     }
-
-    fn profile(&self) -> Option<Profile> {
-        Some(self.profile.clone())
-    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test_log::test]
-    fn test_default_profiles_env_merged() {
+    fn test_env_merged() {
         figment::Jail::expect_with(|jail| {
             jail.create_file(
                 "PrintNanny.toml",
                 r#"
-            [default]
             name = "default"
             path = "/opt/printnanny/default"
             
-            [default.api_config]
+            [api_config]
             base_path = "https://print-nanny.com"
-            
-            [local]
-            name = "local"
-            path = "/home/leigh/projects/print-nanny-cli/.tmp"
-            
-            [local.api_config]
-            base_path = "http://aurora:8000"
             "#,
             )?;
-            jail.set_env("PRINTNANNY_API_CONFIG.API_URL", "http://localhost:8000");
             let figment = Config::figment();
             let config: Config = figment.extract()?;
-            assert_eq!(config.profile, "default");
             assert_eq!(
                 config.api_config,
                 ApiConfig {
@@ -156,7 +156,6 @@ mod tests {
             jail.set_env("PRINTNANNY_API_CONFIG.BEARER_ACCESS_TOKEN", "secret");
             let figment = Config::figment();
             let config: Config = figment.extract()?;
-            assert_eq!(config.profile, "default");
             assert_eq!(
                 config.api_config,
                 ApiConfig {
@@ -169,36 +168,77 @@ mod tests {
     }
 
     #[test_log::test]
-    fn test_custom_profile_selected() {
+    fn test_custom_config_file() {
         figment::Jail::expect_with(|jail| {
             jail.create_file(
-                "PrintNanny.toml",
+                "Local.toml",
                 r#"
-            [default]
-            name = "default"
-            path = "/opt/printnanny/default"
-            
-            [default.api_config]
-            base_path = "https://print-nanny.com"
-            
-            [local]
             name = "local"
             path = "/home/leigh/projects/print-nanny-cli/.tmp"
             
-            [local.api_config]
+            [api_config]
             base_path = "http://aurora:8000"
             "#,
             )?;
-            jail.set_env("PRINTNANNY_API_CONFIG.API_URL", "http://localhost:8000");
-            jail.set_env("PRINTNANNY_PROFILE", "local");
+            jail.set_env("PRINTNANNY_CONFIG", "Local.toml");
+
             let figment = Config::figment();
             let config: Config = figment.extract()?;
-            assert_eq!(config.profile, "local");
+
+            let base_path = "http://aurora:8000".into();
+            let path: String = "/home/leigh/projects/print-nanny-cli/.tmp".into();
+            assert_eq!(config.path, path);
+            assert_eq!(config.api_config.base_path, base_path);
+
+            assert_eq!(
+                config.api_config,
+                ApiConfig {
+                    base_path: base_path,
+                    bearer_access_token: None
+                }
+            );
+            Ok(())
+        });
+    }
+
+    #[test_log::test]
+    fn test_custom_json_toml_glob() {
+        figment::Jail::expect_with(|jail| {
+            jail.create_file(
+                "Base.toml",
+                r#"
+            path = ".tmp"
+            
+            [api_config]
+            base_path = "https://print-nanny.com"
+            "#,
+            )?;
+            let tmp_directory = jail.directory();
+            std::fs::create_dir(tmp_directory.join(".tmp")).unwrap();
+            jail.create_file(
+                ".tmp/ApiConfig.toml",
+                r#"
+            [api_config]
+            base_path = "http://aurora:8000"
+            bearer_access_token = "abc123"
+            "#,
+            )?;
+            // jail.create_file(
+            //     ".tmp/device.json",
+            //     r#"
+            // "#,
+            // )?;
+            jail.set_env("PRINTNANNY_CONFIG", "Base.toml");
+            let figment = Config::figment();
+            let config: Config = figment.extract()?;
+            info!("Read config {:?}", config);
+            // assert_eq!(config.api_config.bearer_access_token, "local");
+            // assert_eq!(config.device.unwrap().id, 1);
             assert_eq!(
                 config.api_config,
                 ApiConfig {
                     base_path: "http://aurora:8000".into(),
-                    bearer_access_token: None
+                    bearer_access_token: Some("abc123".into())
                 }
             );
             Ok(())
