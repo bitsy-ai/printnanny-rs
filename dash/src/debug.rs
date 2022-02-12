6011a3c629e91589f62abbda3ccc31b914aaf23c
Leigh Johnson <releases@bitsy.ai>
2022-02-12 13:58:45 -0800

    replace PrintNannyConfig with static lifetime (#98)

--- dash/src/debug.rs
+++ dash/src/debug.rs
@@ -1,13 +1,17 @@
-use crate::response::Response;
+use crate::auth::PrintNannyConfigFile;
+use crate::response::{FlashResponse, Response};
 use printnanny_services::config::PrintNannyConfig;
 use rocket::serde::json::Json;
 use rocket::State;
+use rocket_dyn_templates::Template;
 
 #[get("/")]
-fn get_config(config: &State<PrintNannyConfig>) -> Response {
+fn get_config(
+    config_file: &State<PrintNannyConfigFile>,
+) -> Result<Response, FlashResponse<Template>> {
+    let config = PrintNannyConfig::new(config_file.0.as_deref())?;
     info!("Rendering config {:?}", config);
-    let c = config.inner().clone();
-    Response::PrintNannyConfig(Json(c))
+    Ok(Response::PrintNannyConfig(Json(config)))
 }
 
 pub fn routes() -> Vec<rocket::Route> {
