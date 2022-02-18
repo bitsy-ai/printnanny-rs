2e1945ffdd24201172dc89e72b31ce4f1da1d5c1
Release Automation <leigh@bitsy.ai>
2022-02-17 21:20:58 -0800

    assert user cookie == device.user

--- dash/src/home.rs
+++ dash/src/home.rs
@@ -1,19 +1,27 @@
 use log::{info, warn};
 use rocket::http::CookieJar;
 use rocket::response::Redirect;
+use rocket::State;
 use rocket_dyn_templates::Template;
 
 use super::auth;
 use super::response::Response;
+use crate::auth::PrintNannyConfigFile;
+use printnanny_api_client::models::User;
 use printnanny_services::config::PrintNannyConfig;
 
 #[get("/")]
-async fn index(jar: &CookieJar<'_>) -> Result<Response, Response> {
-    let api_config = jar.get_private(auth::COOKIE_USER);
-    match api_config {
-        Some(cookie) => {
-            let config: PrintNannyConfig = serde_json::from_str(cookie.value())?;
-            info!("Attaching context to view {:?}", config);
+async fn index(
+    jar: &CookieJar<'_>,
+    config_file: &State<PrintNannyConfigFile>,
+) -> Result<Response, Response> {
+    let cookie = jar.get_private(auth::COOKIE_USER);
+    match cookie {
+        Some(user_json) => {
+            let user: User = serde_json::from_str(user_json.value())?;
+            let config = PrintNannyConfig::new(config_file.0.as_deref())?;
+            assert_eq!(config.user, Some(user));
+            info!("Attaching context to view {:?}", &config);
             Ok(Response::Template(Template::render("index", config)))
         }
         None => {
