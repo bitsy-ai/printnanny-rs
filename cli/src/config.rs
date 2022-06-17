68c8f25cbf049fbfae746a67fe9aa6c5e4933c76
Leigh Johnson <leigh@bitsy.ai>
2022-06-17 15:05:31 -0700

    fix api_service using no credentials in device_setup()

--- cli/src/config.rs
+++ cli/src/config.rs
@@ -52,11 +52,11 @@ impl ConfigAction {
 
 pub async fn handle_check_license(infile: &str) -> Result<(), ServiceError> {
     let mut config: PrintNannyConfig = PrintNannyConfig::new()?;
-    let mut api_service = ApiService::new(config.clone())?;
+    let api_service = ApiService::new(config.clone())?;
     info!("Reading license from {}", infile);
     config.api = api_service.check_license(infile).await?;
     config.try_save_by_key("api")?;
-    api_service.config = config;
+    let mut api_service = ApiService::new(config.clone())?;
     api_service.device_setup().await?;
     Ok(())
 }
