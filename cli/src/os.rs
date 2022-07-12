2f430382a612994c5bc880096424371e56c436e1
Leigh Johnson <leigh@bitsy.ai>
2022-07-12 14:53:28 -0700

    Implement message of the day command handler (#259)
    * implement message of the day command handler
    
    * fix ascii spacing

--- cli/src/os.rs
+++ cli/src/os.rs
@@ -5,6 +5,18 @@ use std::fs;
 
 pub struct OsCommand;
 
+const MTOD_HEADER: &str = r"
+
+_____      _       _   _   _                         
+|  __ \    (_)     | | | \ | |                        
+| |__) | __ _ _ __ | |_|  \| | __ _ _ __  _ __  _   _ 
+|  ___/ '__| | '_ \| __| . ` |/ _` | '_ \| '_ \| | | |
+| |   | |  | | | | | |_| |\  | (_| | | | | | | | |_| |
+|_|   |_|  |_|_| |_|\__|_| \_|\__,_|_| |_|_| |_|\__, |
+                                                 __/ |
+                                                |___/ 
+";
+
 fn handle_issue() -> Result<()> {
     let config = PrintNannyConfig::new()?;
     let result = fs::read_to_string(&config.paths.issue_txt);
@@ -26,10 +38,16 @@ fn handle_issue() -> Result<()> {
     Ok(())
 }
 
+fn handle_motd() -> Result<()> {
+    print!("{}", &MTOD_HEADER);
+    handle_issue()
+}
+
 impl OsCommand {
     pub fn handle(sub_m: &clap::ArgMatches) -> Result<()> {
         match sub_m.subcommand() {
             Some(("issue", _args)) => handle_issue(),
+            Some(("motd", _args)) => handle_motd(),
             _ => Err(anyhow!("Unhandled subcommand")),
         }
     }
