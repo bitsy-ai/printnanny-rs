560c60db4d5d6ae6ebd7f490bc86a6988afd0ad7
Leigh Johnson <leigh@bitsy.ai>
2022-08-19 15:41:13 -0700

    Handle SystemctlShow and SyncSettings commands (#286)
    * parse output of systemctl show command
    
    * finish implementing request/reply
    
    * handle sync settings command

--- nats/src/commands.rs
+++ nats/src/commands.rs
@@ -3,12 +3,14 @@ use async_process::Command;
 use bytes::Bytes;
 use chrono::prelude::{DateTime, Utc};
 use log::{debug, warn};
+use printnanny_api_client::models::{self, PolymorphicPiEventRequest};
+use printnanny_services::config::PrintNannyConfig;
+use printnanny_services::swupdate::Swupdate;
 use std::collections::HashMap;
 use std::time::SystemTime;
 use uuid::Uuid;
 
-use printnanny_api_client::models::{self, PolymorphicPiEventRequest};
-use printnanny_services::swupdate::Swupdate;
+use crate::util::systemctl_show_payload;
 
 pub fn build_status_payload(request: &PolymorphicPiEventRequest) -> Result<Bytes> {
     Ok(serde_json::ser::to_vec(request)?.into())
@@ -24,7 +26,8 @@ pub fn build_boot_status_payload(
     let subject = format!("pi.{pi_id}.status.boot", pi_id = cmd.pi);
     let id = Some(Uuid::new_v4().to_string());
     let created_dt: DateTime<Utc> = SystemTime::now().into();
-    let created_dt = Some(created_dt.to_string());
+    let created_dt = Some(created_dt.to_rfc3339());
+
     let request = PolymorphicPiEventRequest::PiBootStatusRequest(
         models::polymorphic_pi_event_request::PiBootStatusRequest {
             payload,
@@ -106,6 +109,110 @@ pub async fn handle_pi_boot_command(
         }
         models::PiBootCommandType::Shutdown => {
             Command::new("shutdown").output().await?;
+            let (subject, req) =
+                build_boot_status_payload(&cmd, models::PiBootStatusType::ShutdownStarted, None)?;
+
+            // publish to reply topic if present
+            if reply.is_some() {
+                nats_client
+                    .publish(reply.as_ref().unwrap().to_string(), req.clone())
+                    .await?;
+            }
+            // also publish to status topic
+            nats_client.publish(subject.clone(), req).await?;
+
+            debug!(
+                "nats.publish event_type={:?}",
+                models::PiBootStatusType::RebootStarted
+            );
+        }
+
+        models::PiBootCommandType::SystemctlShow => {
+            let result = Command::new("systemctl").arg("show").output().await?;
+
+            let payload = systemctl_show_payload(&result.stdout)?;
+            let (subject, req) = build_boot_status_payload(
+                &cmd,
+                models::PiBootStatusType::SystemctlShow,
+                Some(payload.into()),
+            )?;
+
+            // publish to reply topic if present
+            if reply.is_some() {
+                nats_client
+                    .publish(reply.as_ref().unwrap().to_string(), req.clone())
+                    .await?;
+            }
+            // also publish to status topic
+            nats_client.publish(subject.clone(), req).await?;
+
+            debug!(
+                "nats.publish event_type={:?}",
+                models::PiBootStatusType::RebootStarted
+            );
+        }
+        models::PiBootCommandType::SyncSettings => {
+            // publish SyncSettings event
+            let (subject, req) = build_boot_status_payload(
+                &cmd,
+                models::PiBootStatusType::SyncSettingsStarted,
+                None,
+            )?;
+
+            // publish to reply topic if present
+            if reply.is_some() {
+                nats_client
+                    .publish(reply.as_ref().unwrap().to_string(), req.clone())
+                    .await?;
+            }
+            // also publish to status topic
+            nats_client.publish(subject.clone(), req).await?;
+
+            let config = PrintNannyConfig::new()?;
+            let result = config.sync().await;
+
+            match result {
+                Ok(_) => {
+                    // Publish SyncSettingSuccess event
+                    let (subject, req) = build_boot_status_payload(
+                        &cmd,
+                        models::PiBootStatusType::SyncSettingsSuccess,
+                        None,
+                    )?;
+                    // publish to reply topic if present
+                    if reply.is_some() {
+                        nats_client
+                            .publish(reply.as_ref().unwrap().to_string(), req.clone())
+                            .await?;
+                    }
+                    // also publish to status topic
+                    nats_client.publish(subject.clone(), req).await?;
+                }
+                Err(e) => {
+                    // Publish SyncSettingsError event
+
+                    let mut payload: HashMap<String, serde_json::Value> = HashMap::new();
+                    payload.insert(
+                        "error".to_string(),
+                        serde_json::Value::from(format!("{:?}", e)),
+                    );
+                    let payload = Some(payload);
+                    let (subject, req) = build_boot_status_payload(
+                        &cmd,
+                        models::PiBootStatusType::SyncSettingsError,
+                        payload,
+                    )?;
+
+                    // publish to reply topic if present
+                    if reply.is_some() {
+                        nats_client
+                            .publish(reply.as_ref().unwrap().to_string(), req.clone())
+                            .await?;
+                    }
+                    // also publish to status topic
+                    nats_client.publish(subject.clone(), req).await?;
+                }
+            }
         }
         _ => warn!("No handler configured for msg={:?}", &cmd),
     };
@@ -122,6 +229,7 @@ pub fn build_cam_status_payload(
     let subject = format!("pi.{pi_id}.status.cam", pi_id = cmd.pi);
     let id = Some(Uuid::new_v4().to_string());
     let created_dt: DateTime<Utc> = SystemTime::now().into();
+    let created_dt = Some(created_dt.to_rfc3339());
 
     let request = PolymorphicPiEventRequest::PiCamStatusRequest(
         models::polymorphic_pi_event_request::PiCamStatusRequest {
@@ -129,7 +237,7 @@ pub fn build_cam_status_payload(
             event_type,
             pi: cmd.pi,
             id,
-            created_dt: Some(created_dt.to_string()),
+            created_dt,
         },
     );
     let b = build_status_payload(&request)?;
@@ -283,6 +391,7 @@ pub fn build_swupdate_status_payload(
     let subject = format!("pi.{pi_id}.status.swupdate", pi_id = cmd.pi);
     let id = Some(Uuid::new_v4().to_string());
     let created_dt: DateTime<Utc> = SystemTime::now().into();
+    let created_dt = Some(created_dt.to_rfc3339());
 
     let request = PolymorphicPiEventRequest::PiSoftwareUpdateStatusRequest(
         models::polymorphic_pi_event_request::PiSoftwareUpdateStatusRequest {
@@ -291,7 +400,7 @@ pub fn build_swupdate_status_payload(
             pi: cmd.pi,
             version: cmd.version.clone(),
             id,
-            created_dt: Some(created_dt.to_string()),
+            created_dt,
         },
     );
     let b = build_status_payload(&request)?;
