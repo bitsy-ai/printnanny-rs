975c54031a51aec62a0e57127634913c44db0bb5
Leigh Johnson <leigh@bitsy.ai>
2022-10-09 11:49:53 -0700

    wip: nats worker

--- nats/src/lib.rs
+++ nats/src/lib.rs
@@ -4,3 +4,4 @@ pub mod commands;
 pub mod error;
 pub mod subjects;
 pub mod util;
+pub mod worker;
--- nats/src/worker.rs
+++ nats/src/worker.rs
@@ -1,4 +1,8 @@
-use async_trait::async_trait;
+use std::path::PathBuf;
+
+use clap::{crate_authors, Arg, ArgMatches, Command};
+use log::info;
+
 use super::error::NatsError;
 
 #[derive(Debug, Clone)]
@@ -11,11 +15,12 @@ pub struct NatsWorker {
 }
 
 const DEFAULT_NATS_SOCKET_PATH: &str = "/var/run/printnanny/nats-worker.sock";
+const DEFAULT_NATS_URI: &str = "nats://localhost:4222";
+const DEFAULT_NATS_SUBJECT: &str = "pi.*";
 
-#[async_trait]
-trait NatsWorker {
+impl NatsWorker {
     type NatsEvent;
-    pub fn clap_command() -> Command<'static> {
+    fn clap_command() -> Command<'static> {
         let app_name = "nats-worker";
         let app = Command::new(app_name)
             .author(crate_authors!())
@@ -24,50 +29,55 @@ trait NatsWorker {
                 Arg::new("subject")
                     .long("subject")
                     .takes_value(true)
-                    .default_value("pi.*"),
+                    .default_value(DEFAULT_NATS_SUBJECT),
             )
             .arg(
                 Arg::new("nats_server_uri")
                     .long("nats-server-uri")
                     .takes_value(true)
-                    .default_value("nats://localhost:4222"),
+                    .default_value(DEFAULT_NATS_URI),
             )
-            .arg(
-                Arg::new("nats_creds")
-                    .long("nats-creds")
-                    .takes_value(true)
-            )
+            .arg(Arg::new("nats_creds").long("nats-creds").takes_value(true))
             .arg(
                 Arg::new("socket")
                     .long("socket")
                     .takes_value(true)
-                    .default_value(DEFAULT_NATS_SOCKET_PATH)
+                    .default_value(DEFAULT_NATS_SOCKET_PATH),
             );
         app
     }
-    
-    pub async fn new(args: &ArgMatches) -> Result<Self> {
 
-
-        let subscribe_subject = to_nats_command_subscribe_subject(&pi.id);
+    async fn new(args: &ArgMatches) -> Self {
+        let subject: &str = args
+            .value_of_t("subject")
+            .unwrap_or_else(|_| DEFAULT_NATS_SUBJECT);
 
         // check if uri requires tls
-        let require_tls = nats_app.nats_server_uri.contains("tls");
+        let nats_server_uri: &str = args
+            .value_of_t("nats_server_uri")
+            .unwrap_or_else(|_| DEFAULT_NATS_URI);
+
+        let require_tls = nats_server_uri.nats_server_uri.contains("tls");
 
         // if nats.creds available, initialize authenticated nats connection
         info!(
             "Attempting to initialize NATS connection to {}",
-            nats_app.nats_server_uri
+            nats_server_uri
         );
-        let nats_creds = config.paths.nats_creds();
 
-        Ok(Self {
-            socket: config.paths.events_socket(),
-            subscribe_subject,
-            nats_server_uri: nats_app.nats_server_uri.clone(),
+        let nats_creds = args.value("nats_creds");
+
+        let socket = args
+            .value_of_t("socket")
+            .unwrap_or_else(|_| DEFAULT_NATS_SOCKET_PATH);
+
+        Self {
+            socket,
+            subject,
+            nats_server_uri,
             nats_creds,
             require_tls,
-        })
+        }
     }
 
     pub async fn deserialize_socket_msg(
@@ -81,8 +91,7 @@ trait NatsWorker {
             length_delimited,
             tokio_serde::formats::SymmetricalJson::<(String, NatsEvent)>::default(),
         );
-        let maybe_msg: Option<(String, NatsEvent)> =
-            deserialized.try_next().await?;
+        let maybe_msg: Option<(String, NatsEvent)> = deserialized.try_next().await?;
 
         match maybe_msg {
             Some((subject, msg)) => {
@@ -102,7 +111,7 @@ trait NatsWorker {
         }
     }
 
-    pub async fn subscribe_nats_subject(&self) -> Result<(), NatsError>{
+    pub async fn subscribe_nats_subject(&self) -> Result<(), NatsError> {
         let mut nats_client: Option<async_nats::Client> = None;
         while nats_client.is_none() {
             match self.try_init_nats_client().await {
@@ -170,31 +179,31 @@ trait NatsWorker {
     }
     pub async fn try_init_nats_client(&self) -> Result<async_nats::Client, std::io::Error> {
         match self.nats_creds {
-            Some(nats_creds) => {
-                match nats_creds.exists() {
-                    true => {
-                        async_nats::ConnectOptions::with_credentials_file(self.nats_creds.clone())
-                            .await?
-                            .require_tls(self.require_tls)
-                            .connect(&self.nats_server_uri)
-                            .await
-                    }
-                    false => {
-                        warn!(
-                            "Failed to read {}. Initializing NATS client without credentials",
-                            self.nats_creds.display()
-                        );
-                        async_nats::ConnectOptions::new()
-                            .require_tls(self.require_tls)
-                            .connect(&self.nats_server_uri)
-                            .await
-                    }
+            Some(nats_creds) => match nats_creds.exists() {
+                true => {
+                    async_nats::ConnectOptions::with_credentials_file(self.nats_creds.clone())
+                        .await?
+                        .require_tls(self.require_tls)
+                        .connect(&self.nats_server_uri)
+                        .await
+                }
+                false => {
+                    warn!(
+                        "Failed to read {}. Initializing NATS client without credentials",
+                        self.nats_creds.display()
+                    );
+                    async_nats::ConnectOptions::new()
+                        .require_tls(self.require_tls)
+                        .connect(&self.nats_server_uri)
+                        .await
                 }
             },
-            None => async_nats::ConnectOptions::new()
-                .require_tls(self.require_tls)
-                .connect(&self.nats_server_uri)
-                .await
+            None => {
+                async_nats::ConnectOptions::new()
+                    .require_tls(self.require_tls)
+                    .connect(&self.nats_server_uri)
+                    .await
+            }
         }
     }
     pub async fn subscribe_event_socket(&self) -> Result<()> {
@@ -252,8 +261,7 @@ trait NatsWorker {
                                 match event_buffer.split_first() {
                                     Some((head, rest)) => {
                                         let (subject, payload) = head;
-                                        let payload: NatsEvent =
-                                            serde_json::from_slice(payload)?;
+                                        let payload: NatsEvent = serde_json::from_slice(payload)?;
                                         warn!("Event buffer is full (max size: {}). Dropping oldest event on subject={} payload={:?}", max_buffer_size, subject, payload);
                                         event_buffer = rest.to_vec();
                                     }
@@ -280,5 +288,4 @@ trait NatsWorker {
         nats_sub_task?;
         Ok(())
     }
-
 }
