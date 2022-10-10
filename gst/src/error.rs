a639dfcf10bedd85b93ebdfa3f082f297bbf7ed4
Leigh Johnson <leigh@bitsy.ai>
2022-10-10 11:06:12 -0700

    Remove gst crate (superceded by gst-plugin crate) (#306)
    * remove gst crate (superceded by gst-plugin crate)
    
    * clean up

--- gst/src/error.rs
+++
@@ -1,5 +0,0 @@
-use derive_more::{Display, Error};
-
-#[derive(Debug, Display, Error)]
-#[display(fmt = "Missing gstreamer element {}", _0)]
-pub struct MissingElement(#[error(not(source))] pub &'static str);
--- gst/src/lib.rs
+++
@@ -1,4 +0,0 @@
-pub mod error;
-pub mod h264_video;
-pub mod options;
-pub mod pipeline;
