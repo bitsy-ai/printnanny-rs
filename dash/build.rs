63cb81af55174fb60a61c79060403d722cc8cdde
Leigh Johnson <leigh@bitsy.ai>
2022-10-10 13:54:54 -0700

    try ./ dir path

--- dash/build.rs
+++ dash/build.rs
@@ -1,11 +1,11 @@
 use static_files::NpmBuild;
 
 fn main() -> std::io::Result<()> {
-    NpmBuild::new("ui")
+    NpmBuild::new("./ui")
         .executable("npm")
         .install()?
         .run("build")?
-        .target("ui/dist")
+        .target("./ui/dist")
         .to_resource_dir()
         .build()
 }
