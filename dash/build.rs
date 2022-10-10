026c61a01bb0cb146536aeae280c0e463020e4cb
Leigh Johnson <leigh@bitsy.ai>
2022-10-10 15:50:38 -0700

    try dist dir

--- dash/build.rs
+++ dash/build.rs
@@ -1,17 +1,13 @@
 use static_files::NpmBuild;
-use std::path::PathBuf;
 
 fn main() -> std::io::Result<()> {
-    let base_dir = env!("CARGO_MANIFEST_DIR");
-    let ui_dir = PathBuf::from(base_dir).join("ui");
-    let dist_dir = ui_dir.join("dist");
-    NpmBuild::new(ui_dir)
+    NpmBuild::new("./ui")
         .executable("npm")
         .install()
         .expect("Failed to run npm install")
         .run("build")
         .expect("Failed to run npm build")
-        .target(dist_dir)
+        .target("./ui/dist")
         .to_resource_dir()
         .build()
 }
