30d09dbdaf254aeb89d65a241f7f70987fac5e8c
Leigh Johnson <leigh@bitsy.ai>
2022-11-20 17:04:59 -0800

    clean up

--- nats/src/cloud_commands.rs
+++ nats/src/cloud_commands.rs
@@ -161,10 +161,6 @@ pub async fn handle_pi_boot_command(
 
             //  publish to status topic
             nats_client.publish(subject.clone(), req).await?;
-
-            // let config = PrintNannySettings::new()?;
-            // let result = config.sync().await;
-
             let mut api = ApiService::new()?;
             let result = api.sync().await;
 
