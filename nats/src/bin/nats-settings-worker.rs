0a6057b0094b418e5f8599449f8b23c1f33ac7a5
Leigh Johnson <leigh@bitsy.ai>
2022-11-30 19:01:50 -0800

    passing: test_load_printnanny_settings

--- nats/src/bin/nats-settings-worker.rs
+++ nats/src/bin/nats-settings-worker.rs
@@ -1,13 +1,13 @@
 use anyhow::Result;
-use printnanny_nats::message_v2::{SettingsReply, SettingsRequest};
+use printnanny_nats::message_v2::{NatsReply, NatsRequest};
 use printnanny_nats::subscriber::NatsSubscriber;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
 
-    let app = NatsSubscriber::<SettingsRequest, SettingsReply>::clap_command(None);
-    let worker = NatsSubscriber::<SettingsRequest, SettingsReply>::new(&app.get_matches());
+    let app = NatsSubscriber::<NatsRequest, NatsReply>::clap_command(None);
+    let worker = NatsSubscriber::<NatsRequest, NatsReply>::new(&app.get_matches());
     worker.run().await?;
     Ok(())
 }
--- nats/src/message_v2.rs
+++ nats/src/message_v2.rs
@@ -30,7 +30,7 @@ pub trait NatsReplyBuilder {
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub enum SettingsRequest {
+pub enum NatsRequest {
     #[serde(rename = "pi.{pi}.settings.printnanny.cloud.auth")]
     PrintNannyCloudAuthRequest(PrintNannyCloudAuthRequest),
     #[serde(rename = "pi.settings.printnanny.load")]
@@ -62,7 +62,7 @@ pub enum SettingsRequest {
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub enum SettingsReply {
+pub enum NatsReply {
     #[serde(rename = "pi.{pi_id}.settings.printnanny.cloud.auth")]
     PrintNannyCloudAuthReply(PrintNannyCloudAuthReply),
     #[serde(rename = "pi.settings.printnanny.load")]
@@ -94,18 +94,18 @@ pub enum SettingsReply {
 }
 
 #[async_trait]
-impl NatsReplyBuilder for SettingsReply {
-    type Request = SettingsRequest;
-    type Reply = SettingsReply;
+impl NatsReplyBuilder for NatsReply {
+    type Request = NatsRequest;
+    type Reply = NatsReply;
 
     // async fn build_reply(&self, request: Self::Request) -> Result<Self::Reply> {}
 }
 
-impl SettingsRequest {
+impl NatsRequest {
     pub fn handle_printnanny_settings_load(
         &self,
         request: &SettingsLoadRequest,
-    ) -> Result<SettingsReply> {
+    ) -> Result<NatsReply> {
         let settings = PrintNannySettings::new()?;
         let head_git_commit = settings.get_git_head_commit()?.oid;
         let git_history: Vec<printnanny_asyncapi_models::GitCommit> =
@@ -117,23 +117,72 @@ impl SettingsRequest {
             head_git_commit,
             git_history,
         };
-        Ok(SettingsReply::PrintNannySettingsLoadReply(reply))
+        Ok(NatsReply::PrintNannySettingsLoadReply(reply))
     }
 }
 
 #[async_trait]
-impl NatsRequestHandler for SettingsRequest {
-    type Request = SettingsRequest;
-    type Reply = SettingsReply;
+impl NatsRequestHandler for NatsRequest {
+    type Request = NatsRequest;
+    type Reply = NatsReply;
 
     async fn handle(&self) -> Result<Self::Reply> {
         let reply = match self {
-            SettingsRequest::PrintNannySettingsLoadRequest(request) => {
+            NatsRequest::PrintNannySettingsLoadRequest(request) => {
                 self.handle_printnanny_settings_load(request)?
             }
             _ => todo!(),
         };
 
         Ok(reply)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+    use test_log::test;
+    use tokio::runtime::Runtime;
+
+    fn make_settings_repo(jail: &mut figment::Jail) -> () {
+        let output = jail.directory().to_str().unwrap();
+
+        jail.create_file(
+            "PrintNannySettingsTest.toml",
+            &format!(
+                r#"
+            [paths]
+            settings_dir = "{output}/settings"
+            log_dir = "{output}/log"
+            "#,
+                output = &output
+            ),
+        )
+        .unwrap();
+        jail.set_env("PRINTNANNY_SETTINGS", "PrintNannySettingsTest.toml");
+        let settings = PrintNannySettings::new().unwrap();
+        settings.octoprint.git_clone().unwrap();
+        settings.octoprint.init_local_git_config().unwrap();
+    }
+
+    #[test]
+    fn test_load_printnanny_settings() {
+        let request = NatsRequest::PrintNannySettingsLoadRequest(SettingsLoadRequest {
+            format: Box::new(printnanny_asyncapi_models::SettingsFormat::Toml),
+            filename: Box::new(printnanny_asyncapi_models::SettingsFile::PrintnannyDotToml),
+        });
+        figment::Jail::expect_with(|jail| {
+            make_settings_repo(jail);
+            let reply = Runtime::new().unwrap().block_on(request.handle()).unwrap();
+            if let NatsReply::PrintNannySettingsLoadReply(reply) = reply {
+                let settings = PrintNannySettings::new().unwrap();
+                let expected = settings.to_toml_string().unwrap();
+                assert_eq!(reply.content, expected);
+            } else {
+                panic!("Expected NatsReply::PrintNannySettingsLoadReply")
+            }
+            Ok(())
+        });
     }
 }
