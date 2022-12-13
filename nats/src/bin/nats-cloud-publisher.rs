0d268d5a9b8c919fa9de54d6b144db8c3b8ec374
Leigh Johnson <leigh@bitsy.ai>
2022-12-13 09:54:06 -0800

    fix clippy

--- nats/src/bin/nats-cloud-publisher.rs
+++ nats/src/bin/nats-cloud-publisher.rs
@@ -1,11 +1,11 @@
 use anyhow::Result;
-use printnanny_nats::cloud_publisher:: CloudEventPublisher;
+use printnanny_nats::cloud_publisher::CloudEventPublisher;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
-    let app =  CloudEventPublisher::clap_command();
-    let publisher =  CloudEventPublisher::new(&app.get_matches())?;
+    let app = CloudEventPublisher::clap_command(None);
+    let publisher = CloudEventPublisher::new(&app.get_matches())?;
     publisher.run().await?;
     Ok(())
 }
