1216bd78c3377ba21002c6d16efcdfbade15b791
Leigh Johnson <leigh@bitsy.ai>
2023-01-16 13:33:39 -0800

    interpipesrc/interpipesink names not being unique

--- gst-pipelines/src/factory.rs
+++ gst-pipelines/src/factory.rs
@@ -22,6 +22,14 @@ impl PrintNannyPipelineFactory {
         format!("http://{}:{}", address, port)
     }
 
+    fn to_interpipesrc_name(pipeline_name: &str) -> String {
+        format!("{pipeline_name}_src")
+    }
+
+    fn to_interpipesink_name(pipeline_name: &str) -> String {
+        format!("{pipeline_name}_sink")
+    }
+
     async fn make_pipeline(
         &self,
         pipeline_name: &str,
@@ -60,10 +68,11 @@ impl PrintNannyPipelineFactory {
         pipeline_name: &str,
         camera: &CameraSettings,
     ) -> Result<gst_client::resources::Pipeline> {
+        let interpipesink = Self::to_interpipesink_name(pipeline_name);
         let description = format!(
             "libcamerasrc camera-name={camera_name} \
             ! capsfilter caps=video/x-raw,width=(int){width},height=(int){height},framerate=(fraction){framerate}/1 \
-            ! interpipesink name={pipeline_name} sync=false",
+            ! interpipesink name={interpipesink} sync=false",
             camera_name=camera.device_name,
             // pixel_format=camera.caps.
             width=camera.width,
@@ -79,7 +88,11 @@ impl PrintNannyPipelineFactory {
         listen_to: &str,
         filesink_location: &str,
     ) -> Result<gst_client::resources::Pipeline> {
-        let description = format!("interpipesrc name={pipeline_name} listen-to={listen_to} accept-events=false accept-eos-event=false is-live=true allow-renegotiation=false num-buffers=2 leaky-type=2 \
+
+        let interpipesrc = Self::to_interpipesrc_name(pipeline_name);
+        let listen_to = Self::to_interpipesink_name(listen_to);
+
+        let description = format!("interpipesrc name={interpipesrc} listen-to={listen_to} accept-events=false accept-eos-event=false is-live=true allow-renegotiation=false num-buffers=2 leaky-type=2 \
             ! v4l2jpegenc ! multifilesink max-files=2 location={filesink_location}");
         self.make_pipeline(pipeline_name, &description).await
     }
@@ -90,12 +103,17 @@ impl PrintNannyPipelineFactory {
         listen_to: &str,
         framerate: &i32,
     ) -> Result<gst_client::resources::Pipeline> {
-        let description = format!("interpipesrc name={pipeline_name} listen-to={listen_to} accept-events=false accept-eos-event=false is-live=true allow-renegotiation=false \
+
+        let listen_to = Self::to_interpipesink_name(listen_to);
+        let interpipesrc = Self::to_interpipesrc_name(pipeline_name);
+        let interpipesink = Self::to_interpipesink_name(pipeline_name);
+
+        let description = format!("interpipesrc name={interpipesrc} listen-to={listen_to} accept-events=false accept-eos-event=false is-live=true allow-renegotiation=false \
             ! v4l2convert \
             ! v4l2h264enc min-force-key-unit-interval={framerate} extra-controls=controls,repeat_sequence_header=1 \
             ! h264parse \
             ! capsfilter caps=video/x-h264,level=(string)3,profile=(string)main \
-            ! interpipesink name={pipeline_name} sync=false");
+            ! interpipesink name={interpipesink} sync=false");
         self.make_pipeline(pipeline_name, &description).await
     }
 
@@ -105,7 +123,11 @@ impl PrintNannyPipelineFactory {
         listen_to: &str,
         port: i32,
     ) -> Result<gst_client::resources::Pipeline> {
-        let description = format!("interpipesrc name={pipeline_name} listen-to={listen_to} accept-events=false accept-eos-event=false is-live=true allow-renegotiation=false \
+
+        let listen_to = Self::to_interpipesink_name(listen_to);
+        let interpipesrc = Self::to_interpipesrc_name(pipeline_name);
+
+        let description = format!("interpipesrc name={interpipesrc} listen-to={listen_to} accept-events=false accept-eos-event=false is-live=true allow-renegotiation=false \
             ! rtph264pay config-interval=1 aggregate-mode=zero-latency pt=96 \
             ! udpsink port={port}");
         self.make_pipeline(pipeline_name, &description).await
@@ -119,7 +141,11 @@ impl PrintNannyPipelineFactory {
         hls_playlist_location: &str,
         hls_playlist_root: &str,
     ) -> Result<gst_client::resources::Pipeline> {
-        let description = format!("interpipesrc name={pipeline_name} listen-to={listen_to} accept-events=false accept-eos-event=false is-live=true allow-renegotiation=false \
+
+        let listen_to = Self::to_interpipesink_name(listen_to);
+        let interpipesrc = Self::to_interpipesrc_name(pipeline_name);
+
+        let description = format!("interpipesrc name={interpipesrc} listen-to={listen_to} accept-events=false accept-eos-event=false is-live=true allow-renegotiation=false \
             ! hlssink2 paylist-length=8 max-files=10 target-duration=1 location={hls_segments_location} playlist-location={hls_playlist_location} playlist-root={hls_playlist_root} send-keyframe-requests=false");
         self.make_pipeline(pipeline_name, &description).await
     }
@@ -132,13 +158,18 @@ impl PrintNannyPipelineFactory {
         tensor_height: i32,
         tflite_model_file: &str,
     ) -> Result<gst_client::resources::Pipeline> {
-        let description = format!("interpipesrc name={pipeline_name} listen-to={listen_to} accept-events=false accept-eos-event=false is-live=true allow-renegotiation=false num-buffers=2 leaky-type=2 \
+
+        let listen_to = Self::to_interpipesink_name(listen_to);
+        let interpipesrc = Self::to_interpipesrc_name(pipeline_name);
+        let interpipesink = Self::to_interpipesink_name(pipeline_name);
+
+        let description = format!("interpipesrc name={interpipesrc} listen-to={listen_to} accept-events=false accept-eos-event=false is-live=true allow-renegotiation=false num-buffers=2 leaky-type=2 \
             ! videoconvert ! videoscale ! capsfilter caps=video/x-raw,format=RGB,width={tensor_width},height={tensor_height} \
             ! tensor_converter \
             ! tensor_transform mode=arithmetic option=typecast:uint8,add:0,div:1 \
             ! capsfilter caps=other/tensors,format=static \
             ! tensor-filter framework=tensorflow2-lite model={tflite_model_file} \
-            ! interpipesink name={pipeline_name} sync=false");
+            ! interpipesink name={interpipesink} sync=false");
         self.make_pipeline(pipeline_name, &description).await
     }
 
@@ -154,7 +185,11 @@ impl PrintNannyPipelineFactory {
         tflite_label_file: &str,
         port: i32,
     ) -> Result<gst_client::resources::Pipeline> {
-        let description = format!("interpipesrc name={pipeline_name} listen-to={listen_to} accept-events=false accept-eos-event=false is-live=true allow-renegotiation=false \
+
+        let listen_to = Self::to_interpipesink_name(listen_to);
+        let interpipesrc = Self::to_interpipesrc_name(pipeline_name);
+
+        let description = format!("interpipesrc name={interpipesrc} listen-to={listen_to} accept-events=false accept-eos-event=false is-live=true allow-renegotiation=false \
             ! tensor_decoder mode=bounding_boxes option1=mobilenet-ssd-postprocess option2={tflite_label_file} option3=0:1:2:3,{nms_threshold} option4={video_width}:{video_height} option5={tensor_width}:{tensor_height} \
             ! videoconvert \
             ! v4l2h264enc output-io-mode=mmap capture-io-mode=mmap extra-controls=controls,repeat_sequence_header=1 \
@@ -174,7 +209,11 @@ impl PrintNannyPipelineFactory {
         nats_server_uri: &str,
     ) -> Result<gst_client::resources::Pipeline> {
         let nms_threshold = nms_threshold as f32 / 100_f32;
-        let description = format!("interpipesrc name={pipeline_name} listen-to={listen_to} accept-events=false accept-eos-event=false is-live=true allow-renegotiation=false \
+
+        let listen_to = Self::to_interpipesink_name(listen_to);
+        let interpipesrc = Self::to_interpipesrc_name(pipeline_name);
+
+        let description = format!("interpipesrc name={interpipesrc} listen-to={listen_to} accept-events=false accept-eos-event=false is-live=true allow-renegotiation=false \
             ! tensor_decoder mode=custom-code option1=printnanny_bb_dataframe_decoder \
             ! dataframe_agg filter-threshold={nms_threshold} output-type=json |
             ! nats_sink nats-address={nats_server_uri}");
