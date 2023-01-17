d5595082f118a348cd2f4a0371321dc51d8d9195
Leigh Johnson <leigh@bitsy.ai>
2023-01-16 20:43:57 -0800

    add recording status (#446)

--- db/src/schema.rs
+++ db/src/schema.rs
@@ -3,10 +3,12 @@
 diesel::table! {
     video_recordings (id) {
         id -> Binary,
+        recording_status -> Text,
         recording_start -> Nullable<Integer>,
         recording_end -> Nullable<Integer>,
         recording_file_name -> Text,
         gcode_file_name -> Nullable<Text>,
+        cloud_sync_status -> Text,
         cloud_sync_start -> Nullable<Integer>,
         cloud_sync_end -> Nullable<Integer>,
     }
