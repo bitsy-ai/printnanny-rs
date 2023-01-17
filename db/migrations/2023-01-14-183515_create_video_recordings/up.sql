8a8e6f733337d2dea3b59857581701cd9cc04955
Leigh Johnson <leigh@bitsy.ai>
2023-01-16 20:38:09 -0800

    add recording status

--- db/migrations/2023-01-14-183515_create_video_recordings/up.sql
+++ db/migrations/2023-01-14-183515_create_video_recordings/up.sql
@@ -1,9 +1,11 @@
 CREATE TABLE video_recordings (
   id blob(16) PRIMARY KEY NOT NULL,
+  recording_status VARCHAR NOT NULL,
   recording_start INTEGER,
   recording_end INTEGER,
   recording_file_name VARCHAR NOT NULL,
   gcode_file_name VARCHAR,
+  cloud_sync_status VARCHAR NOT NULL,
   cloud_sync_start INTEGER,
   cloud_sync_end INTEGER
 )
--- db/src/models.rs
+++ db/src/models.rs
@@ -1,12 +1,31 @@
 use diesel::prelude::*;
+use diesel_enum::DbEnum;
+
+#[derive(Debug)]
+pub struct EnumError {
+    msg: String,
+    status: u16,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, AsExpression, FromSqlRow, DbEnum)]
+#[sql_type = "VarChar"]
+#[error_fn = "EnumError::not_found"]
+#[error_type = "EnumError"]
+pub enum Status {
+    Pending,
+    InProgress,
+    Done,
+}
 
 #[derive(Queryable)]
 pub struct VideoRecording {
     pub id: diesel::sql_types::Uuid,
+    pub recording_status: Status,
     pub recording_start: Option<u64>,
     pub recording_end: Option<u64>,
     pub recording_file_name: String,
     pub gcode_file_name: Option<String>,
+    pub cloud_sync_status: Status,
     pub cloud_sync_start: Option<u64>,
     pub cloud_sync_end: Option<u64>,
 }
