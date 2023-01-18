87a0901396315ae112bb8880d04775f7fd023840
Leigh Johnson <leigh@bitsy.ai>
2023-01-18 09:18:37 -0800

    embed migrations in binary, run pending migrations with printnanny_os_init() fn

--- services/src/setup.rs
+++ services/src/setup.rs
@@ -1,3 +1,4 @@
+use printnanny_edge_db::connection::run_migrations;
 use printnanny_settings::printnanny::PrintNannySettings;
 
 use crate::error::ServiceError;
@@ -7,5 +8,7 @@ pub fn printnanny_os_init() -> Result<(), ServiceError> {
     let settings = PrintNannySettings::new()?;
     // ensure directory structure exists
     settings.paths.try_init_all()?;
+    // run any pending migrations
+    run_migrations()?;
     Ok(())
 }
