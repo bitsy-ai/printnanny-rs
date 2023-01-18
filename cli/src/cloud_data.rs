d28ce4e944dd750ecfa79730f81c56946ffcafa6
Leigh Johnson <leigh@bitsy.ai>
2023-01-18 10:38:02 -0800

    clippy

--- cli/src/cloud_data.rs
+++ cli/src/cloud_data.rs
@@ -1,6 +1,5 @@
 use printnanny_services::error::ServiceError;
 use printnanny_services::printnanny_api::ApiService;
-use printnanny_settings::SettingsFormat;
 use std::io::{self, Write};
 
 use printnanny_edge_db::cloud::Pi;
@@ -14,7 +13,7 @@ impl CloudDataCommand {
                 let mut service = ApiService::new()?;
                 service.sync().await?;
             }
-            Some(("show", args)) => {
+            Some(("show", _args)) => {
                 let pi = Pi::get()?;
                 let v = serde_json::to_vec_pretty(&pi)?;
                 io::stdout().write_all(&v)?;
--- services/src/printnanny_api.rs
+++ services/src/printnanny_api.rs
@@ -246,7 +246,7 @@ impl ApiService {
         }
 
         let pi = self.pi_retrieve(edge_pi.id).await?;
-        let pi_id = pi.id.clone();
+        let pi_id = pi.id;
         let changeset: printnanny_edge_db::cloud::UpdatePi = pi.clone().into();
         printnanny_edge_db::cloud::Pi::update(pi_id, changeset)?;
         Ok(pi)
