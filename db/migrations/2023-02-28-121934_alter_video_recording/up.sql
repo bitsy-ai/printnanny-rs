2287d62fe60d3222c332dc633d8231b61ffb16bc
Leigh Johnson <leigh@bitsy.ai>
2023-03-01 10:58:53 -0800

    rm capture_done and use recording_end

--- db/migrations/2023-02-28-121934_alter_video_recording/up.sql
+++ db/migrations/2023-02-28-121934_alter_video_recording/up.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS video_recordings;
 CREATE TABLE video_recordings (
   id VARCHAR PRIMARY KEY NOT NULL,
-  capture_done BOOLEAN NOT NULL,
   cloud_sync_done BOOLEAN NOT NULL,
   dir VARCHAR NOT NULL,
   recording_start DATETIME,
