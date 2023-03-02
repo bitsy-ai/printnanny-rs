a149216d82d7315a30c0860225016eb19b166f30
Leigh Johnson <leigh@bitsy.ai>
2023-03-02 02:56:18 -0800

    fix clippy

--- gst-pipelines/src/factory.rs
+++ gst-pipelines/src/factory.rs
@@ -1,6 +1,7 @@
 use std::fs;
 
 use anyhow::Result;
+use gst_client::gstd_types;
 use gst_client::reqwest;
 use gst_client::GstClient;
 use log::{error, info, warn};
@@ -383,7 +384,6 @@ impl PrintNannyPipelineFactory {
             pipeline_name
         );
 
-        let mut tasks = vec![]
         let sqlite_connection = settings.paths.db().display().to_string();
         let mut recording =
             printnanny_edge_db::video_recording::VideoRecording::get_current(&sqlite_connection)?;
@@ -391,46 +391,61 @@ impl PrintNannyPipelineFactory {
             let msg = bus.read().await;
             match msg {
                 Ok(msg) => {
-                    info!(
-                        "Handling msg on gstreamer pipeline bus name={} msg={:?}",
-                        pipeline_name, msg
-                    );
-                    // attempt to deserialize msg
-                    let filesink_msg = serde::<GstMultiFileSinkMessage>::from_str(msg.message);
-                    match filesink_msg {
-                        Ok(filesink_msg) => {
-                            // try to get current recording
-                            recording =
-                            printnanny_edge_db::video_recording::VideoRecording::get_current(&sqlite_connection)?;
-                            if recording.is_none() {
-                                warn!("Refusing to process GstMultiFileSink msg, could not find active recording");
-                                continue;
+                    match msg.response {
+                        gstd_types::ResponseT::Bus(Some(msg)) => {
+                            info!(
+                                "Handling msg on gstreamer pipeline bus name={} msg={:?}",
+                                pipeline_name, msg
+                            );
+
+                            // attempt to deserialize msg
+                            let filesink_msg =
+                                serde_json::from_str::<GstMultiFileSinkMessage>(&msg.message);
+                            match filesink_msg {
+                                Ok(filesink_msg) => {
+                                    // try to get current recording
+                                    recording =
+                                    printnanny_edge_db::video_recording::VideoRecording::get_current(&sqlite_connection)?;
+                                    if recording.is_none() {
+                                        warn!("Refusing to process GstMultiFileSink msg, could not find active recording");
+                                        continue;
+                                    }
+                                    let video_recording_id = recording.unwrap().id;
+
+                                    let size = fs::metadata(&filesink_msg.filename)?.len() as i64;
+
+                                    // insert new VideoRecordingPart
+                                    let row_id =
+                                        format!("{video_recording_id}-{}", filesink_msg.index);
+                                    let row = printnanny_edge_db::video_recording::NewVideoRecordingPart{
+                                        id: &row_id,
+                                        buffer_index: &(filesink_msg.index as i32),
+                                        buffer_ts: &(filesink_msg.timestamp as i64),
+                                        buffer_streamtime: &(filesink_msg.streamtime as i64),
+                                        buffer_runningtime: &(filesink_msg.runningtime as i64),
+                                        buffer_duration: &(filesink_msg.duration as i64),
+                                        buffer_offset: &(filesink_msg.offset as i64),
+                                        buffer_offset_end: &(filesink_msg.offset_end as i64),
+                                        deleted: &false,
+                                        file_name: &filesink_msg.filename,
+                                        video_recording_id: &video_recording_id,
+                                        size: &size
+                                    };
+                                    match printnanny_edge_db::video_recording::VideoRecordingPart::insert(&sqlite_connection, row) {
+                                        Ok(()) => info!("Inserted VideoRecordingPart video_recording_id={} id={} file_name={}",&video_recording_id, &row_id, &filesink_msg.filename  ),
+                                        Err(e) => error!("Failed to insert VideoRecordingPart row, error={}", e)
+                                    }
+                                }
+                                Err(e) => {
+                                    error!(
+                                        "Failed to deserialize GstMultiFileSinkMessage from msg={}",
+                                        &msg.message
+                                    );
+                                }
                             }
-                            let video_recording_id = recording.unwrap().id;
-                            // insert new VideoRecordingPart
-                            let row_id = format!("{recording_id}-{}", filesink_msg.index);
-                            let row = printnanny_edge_db::video_recording::NewVideoRecordingPart{
-                                id: row_id,
-                                buffer_index: &filesink_msg.index,
-                                buffer_ts: &filesink_msg.timestamp,
-                                buffer_streamtime: &filesink_msg.streamtime,
-                                buffer_runningtime: &filesink_msg.runningtime,
-                                buffer_duration: &filesink_msg.duration,
-                                buffer_offset: &filesink_msg.offset,
-                                buffer_offset_end: &filesink_msg.offset_end,
-                                deleted: &false,
-                                sync_start: None,
-                                sync_end: None,
-                                file_name: &filesink_msg.filename,
-                                video_recording_id: &video_recording_id
-                            };
-                            
-                        },
-                        Err(e) => {
-                            error!("Failed to deserialize GstMultiFileSinkMessage from msg={}", &msg.message);
                         }
+                        _ => error!("Failed to process response={:#?}", msg.response),
                     }
-                    
                 }
                 Err(e) => {
                     error!("Error reading gstreamer pipeline bus name={} filter=splitmuxsink-fragment-closed error={}", pipeline_name, e);
