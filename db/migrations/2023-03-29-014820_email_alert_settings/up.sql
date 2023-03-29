5451c2021d399f5d3b8837061ab7744d49bb91f5
Leigh Johnson <leigh@bitsy.ai>
2023-03-28 18:55:16 -0700

    generate alert_settings sqlite schema

---
+++ db/migrations/2023-03-29-014820_email_alert_settings/up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE email_alert_settings (
+  id INTEGER PRIMARY KEY NOT NULL,
+  created_dt DATETIME,
+  updated_dt DATETIME,
+  progress_percent UNSIGNED INT NOT NULL,
+  print_quality_enabled BOOLEAN NOT NULL,
+  print_started_enabled BOOLEAN NOT NULL,
+  print_done_enabled BOOLEAN NOT NULL,
+  print_progress_enabled BOOLEAN NOT NULL,
+  print_paused_enabled BOOLEAN NOT NULL,
+  print_cancelled_enabled BOOLEAN NOT NULL
+)
