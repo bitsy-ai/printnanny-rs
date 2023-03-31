fa1f4c16fce0ecae0c85e63a01e68daa608a168c
Leigh Johnson <leigh@bitsy.ai>
2023-03-31 14:14:12 -0700

    permit jpeg snapshot to renegotiate caps

--- gst-pipelines/src/factory.rs
+++ gst-pipelines/src/factory.rs
@@ -232,7 +232,7 @@ impl PrintNannyPipelineFactory {
 
         let max_buffers = 30;
 
-        let description = format!("interpipesrc name={interpipesrc} listen-to={listen_to} accept-events=false accept-eos-event=false is-live=true allow-renegotiation=false max-buffers={max_buffers} leaky-type=2 \
+        let description = format!("interpipesrc name={interpipesrc} listen-to={listen_to} accept-events=false accept-eos-event=false is-live=true allow-renegotiation=true max-buffers={max_buffers} leaky-type=2 \
             ! v4l2jpegenc ! multifilesink location={filesink_location} max-files={max_buffers}",
             // width=camera.width,
             // height=camera.height,
